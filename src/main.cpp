32092917821e1a51e10df0083155099de172b421
Matthew Zimmer <the.matthew.zimmer@gmail.com>
2020-03-11 13:00:12 +0000

    directory setup

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+
+#include <reflection/reflection.hpp>
+#include <reflection/variables.hpp>
+
+//#include <math/math
